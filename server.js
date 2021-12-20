236c58103820a9d4195651151c7097fff911b432
Taruni Patel <74676333+Tarunipatel@users.noreply.github.com>
2021-12-21 00:25:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.use('/static',express.static(path.join))
+app.use('/static',express.static(path.join(__dirname, 'public')))
+app.use('/assets',express.static(path.join(__dirname,'public/assets')))
 app.get('/', (req,res) =>{
 
 
